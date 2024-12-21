15b7f3e65e4546516486665357f008d7e286e4e6
Bobbos Bob <purplelabdev@gmail.com>
2024-12-20 19:01:42 -0500

    I put in the test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1 class="font-semibold text-4xl text-teal-500">FastForm!</h1>
+
+  <h1 class="text-purple-500">Penis</h1>
 </template>
 
 <script>
@@ -9,5 +11,9 @@ export default {
 </script>
 
 <style>
+  h1{
+    font-family: comic sans MS;
+    font-size: 500px;
+  }
 
 </style>
